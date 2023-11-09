2e55057d3267064e052c50bf126cbafe32a848a6
TakutoHashimoto <tkthsmt0531@gmail.com>
2023-11-09 23:08:49 +0900

    add script.py

---
+++ sec4/script.py
@@ -0,0 +1,32 @@
+import requests
+import os
+import pandas as pd
+
+
+URL = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/"
+API_KEY = os.environ.get("API_KEY")
+
+params = {
+    "key": API_KEY,
+    "keyword": "沖縄",
+    "count": 100,
+    "format": "json"
+}
+
+res = requests.get(URL, params)
+print(res.status_code)
+
+result = res.json()
+items = result["results"]["shop"]
+print(len(items))
+
+
+df = pd.DataFrame(items)
+print(df.shape)
+print(df.head())
+
+print(df.columns)
+df = df[["name", "address", "wifi"]]
+print(df.head())
+
+df.to_csv("hotpepper", index=False)
